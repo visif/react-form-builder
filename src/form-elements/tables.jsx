af3a07602fea85de55f84f6caaca9b7246a02625
tao <fortunealphat@gmail.com>
2021-07-26 23:15:18 +0700

    update table object

---
+++ src/form-elements/tables.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+export class Tables extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            message: "",
+            items: []
+        }
+    }
+
+    updateMessage(event) {
+        this.setState({
+            message: event.target.value
+        });
+    }
+
+    handleClick() {
+        var items = this.state.items;
+
+        items.push(this.state.message);
+
+        this.setState({
+            items: items,
+            message: ""
+        });
+    }
+
+    handleItemChanged(i, event) {
+        var items = this.state.items;
+        items[i] = event.target.value;
+
+        this.setState({
+            items: items
+        });
+    }
+
+    handleItemDeleted(i) {
+        var items = this.state.items;
+
+        items.splice(i, 1);
+
+        this.setState({
+            items: items
+        });
+    }
+
+    renderRows() {
+        var context = this;
+
+        return this.state.items.map(function (o, i) {
+            return (
+                <tr key={"item-" + i}>
+                    <td>
+                        <input
+                            type="text"
+                            value={o}
+                            onChange={context.handleItemChanged.bind(context, i)}
+                        />
+                    </td>
+                    <td>
+                        <button
+                            onClick={context.handleItemDeleted.bind(context, i)}
+                        >
+                            Delete
+                        </button>
+                    </td>
+                </tr>
+            );
+        });
+    }
+}
