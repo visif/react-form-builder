3db9794f7f06a0b80242ce821a4527b28eb61a1e
santobbu <santobbu@gmail.com>
2021-11-15 00:58:56 +0700

    Add validation check for signature clicked

--- src/form-elements/signature2.jsx
+++ src/form-elements/signature2.jsx
@@ -5,12 +5,18 @@ const Signature2 = (props) => {
   const [ isSigned, setIsSigned ] = useState(false);
 
   const clickToSign = () => {
-    setIsSigned(!isSigned);
-  }
+    if (typeof props.getActiveUserProperties !== 'function' ) {
+      return;
+    }
+    const userProperties = props.getActiveUserProperties();
 
-  const newProps = {
-    name: props.data.field_name,
-  };
+    if (userProperties && 
+      (`${userProperties.role}`.toLocaleLowerCase() === `${props.data.position}`.toLocaleLowerCase()
+      || `${userProperties.name}`.toLocaleLowerCase() === `${props.data.position}`.toLocaleLowerCase())
+    ) {
+      setIsSigned(!isSigned);
+    }
+  }
 
   let baseClasses = 'SortableItem rfb-item';
   if (props.data.pageBreakBefore) { baseClasses += ' alwaysbreak'; }
@@ -18,7 +24,7 @@ const Signature2 = (props) => {
   return (
     <div className={baseClasses}>
       <ComponentHeader {...props} />
-      <div className="form-group" onClick={clickToSign}>
+      <div className="form-group" onClick={clickToSign} style={{ cursor: 'pointer' }}>
         <h5 style={{ textAlign: 'center' }}>{isSigned ? 'Already signed' : '(Click to sign)'}</h5>
         <div style={{ textAlign: 'center', marginTop: 8, marginBottom: 8 }}>__________________</div>
         <h6 style={{ textAlign: 'center' }}>{props.data.position || 'Placeholder Text'}</h6>
