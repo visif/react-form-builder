3228fae32d8a8e7e51c5a515d8bf76d25168f726
santobbu <santobbu@gmail.com>
2021-11-15 00:09:49 +0700

    Change signature from class to function component

--- src/form-elements/signature2.jsx
+++ src/form-elements/signature2.jsx
@@ -1,34 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ComponentHeader from './component-header';
 
-export default class Signature2 extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isSigned: false,
-    };
+const Signature2 = (props) => {
+  const [ isSigned, setIsSigned ] = useState(false);
+
+  const clickToSign = () => {
+    setIsSigned(!isSigned);
   }
 
-  clickToSign = () => {
-    this.setState({isSigned: !this.state.isSigned});
-  }
+  const newProps = {
+    name: props.data.field_name,
+  };
 
-  render() {
-    const props = {};
-    props.name = this.props.data.field_name;
+  let baseClasses = 'SortableItem rfb-item';
+  if (props.data.pageBreakBefore) { baseClasses += ' alwaysbreak'; }
 
-    let baseClasses = 'SortableItem rfb-item';
-    if (this.props.data.pageBreakBefore) { baseClasses += ' alwaysbreak'; }
-
-    return (
-      <div className={baseClasses}>
-        <ComponentHeader {...this.props} />
-        <div className="form-group" onClick={this.clickToSign}>
-          <div style={{ textAlign: 'center' }}>{this.state.isSigned ? 'Already signed' : '(Click to sign)'}</div>
-          <div style={{ textAlign: 'center', marginTop: 8, marginBottom: 8 }}>__________________</div>
-          <div style={{ textAlign: 'center' }}>{this.props.data.position || 'Placeholder Text'}</div>
-        </div>
+  return (
+    <div className={baseClasses}>
+      <ComponentHeader {...props} />
+      <div className="form-group" onClick={clickToSign}>
+        <h5 style={{ textAlign: 'center' }}>{isSigned ? 'Already signed' : '(Click to sign)'}</h5>
+        <div style={{ textAlign: 'center', marginTop: 8, marginBottom: 8 }}>__________________</div>
+        <h6 style={{ textAlign: 'center' }}>{props.data.position || 'Placeholder Text'}</h6>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Signature2;
