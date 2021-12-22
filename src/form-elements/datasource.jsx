edd550f840c53b84c8b5098c11fe430de6314989
santobbu <santobbu@gmail.com>
2021-12-23 02:32:35 +0700

    Update data source search panel

--- src/form-elements/datasource.jsx
+++ src/form-elements/datasource.jsx
@@ -10,6 +10,10 @@ class DataSource extends React.Component {
 
     this.state = {
       sourceList: [],
+      matchedList: [],
+      searchText: '',
+      isShowingList: false,
+      value: '',
     };
   }
 
@@ -17,9 +21,39 @@ class DataSource extends React.Component {
     if (this.props.getDataSource && typeof this.props.getDataSource === 'function') {
       const data = this.props.getDataSource(this.props.data.sourceType);
       this.setState({
-        sourceList: data
+        sourceList: data,
+        matchedList: data,
       });
     }
+  }
+
+  handleInputFocus = () => {
+    this.setState({
+      isShowingList: true,
+    })
+  }
+
+  handleInputBlur = () => {
+    this.setState({
+      isShowingList: false,
+    })
+  };
+
+  handleOnChange = (event) => {
+    if(event.key === 'Enter'){
+      return;
+    }
+
+    const value = event.target.value;
+    console.log('>>', value)
+
+    const matchData = this.state.sourceList.filter(item => {
+      return `${item}`.toLocaleLowerCase().includes(`${value}`.toLocaleLowerCase())
+    });
+    this.setState({
+      searchText: value,
+      matchedList: matchData,
+    });
   }
 
   render() {
@@ -27,6 +61,8 @@ class DataSource extends React.Component {
     props.type = 'text';
     props.className = 'form-control';
     props.name = this.props.data.field_name;
+    props.value = this.state.value;
+
     if (this.props.mutable) {
       props.defaultValue = this.props.defaultValue;
       props.ref = this.inputField;
@@ -46,30 +82,30 @@ class DataSource extends React.Component {
           <ComponentLabel {...this.props} style={{ display: 'block' }}/>
           <div style={{ position: 'relative', display: 'inline-block', width: '100%' }}>
             <div>
-              <input {...props} />
+              <input 
+                {...props} 
+                onFocus={this.handleInputFocus}
+                onBlur={this.handleInputBlur}
+                onChange={this.handleOnChange} 
+              />
             </div>
-            <div style={{position: 'absolute', zIndex: 99, top: "100%", left: 0, right: 0 }}>
-              <div style={{ 
-                position: 'relative', display: 'block', padding: '0.75rem 1.25rem',
-                marginBottom: -1, backgroundColor: '#fff', border: '1px solid rgba(0, 0, 0, 0.125)' }} >
-                  Test 1
-              </div>
-              <div style={{ 
-                position: 'relative', display: 'block', padding: '0.75rem 1.25rem',
-                marginBottom: -1, backgroundColor: '#fff', border: '1px solid rgba(0, 0, 0, 0.125)' }} >
-                  Test 2
-              </div>
-              <div style={{ 
-                position: 'relative', display: 'block', padding: '0.75rem 1.25rem',
-                marginBottom: -1, backgroundColor: '#fff', border: '1px solid rgba(0, 0, 0, 0.125)' }} >
-                  Test 3
-              </div>
+            <div style={{
+                position: 'absolute', zIndex: 99, top: "100%", left: 0, right: 0,
+                display: this.state.isShowingList ? 'block' : 'none'
+              }}
+            >
               {
-                this.state.sourceList.map(item => {
+                this.state.matchedList.map(item => {
                   return (
                     <div style={{ 
                       position: 'relative', display: 'block', padding: '0.75rem 1.25rem',
-                      marginBottom: -1, backgroundColor: '#fff', border: '1px solid rgba(0, 0, 0, 0.125)' }} >
+                      marginBottom: -1, backgroundColor: '#fff', border: '1px solid rgba(0, 0, 0, 0.125)' }} 
+                      onClick={() => {
+                        this.setState({
+                          value: item,
+                        })
+                      }}
+                    >
                         {item}
                     </div>
                   );
