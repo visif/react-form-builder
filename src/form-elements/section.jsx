be8daef3814a861e44a87ce34a7b16b518eed196
santobbu <santobbu@gmail.com>
2023-03-30 23:37:23 +0700

    Migrate section

--- src/form-elements/section.jsx
+++ src/form-elements/section.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import ComponentHeader from './component-header';
+// import React from 'react';
+// import ComponentHeader from './component-header';
 
-export default class Section extends React.Component {
-  render() {
-    let baseClasses = 'SortableItem rfb-item';
-    if (this.props.data.pageBreakBefore) { baseClasses += ' alwaysbreak'; }
+// export default class Section extends React.Component {
+//   render() {
+//     let baseClasses = 'SortableItem rfb-item';
+//     if (this.props.data.pageBreakBefore) { baseClasses += ' alwaysbreak'; }
 
-    return (
-      <div className={baseClasses}>
-        <ComponentHeader {...this.props} />
-        <h6>{this.props.data.header}</h6>
-        <hr />
-      </div>
-    );
+//     return (
+//       <div className={baseClasses}>
+//         <ComponentHeader {...this.props} />
+//         <h6>{this.props.data.header}</h6>
+//         <hr />
+//       </div>
+//     );
+//   }
+// }
+
+import React from "react";
+import ComponentHeader from "./component-header";
+
+const Section = (props) => {
+  let baseClasses = "SortableItem rfb-item";
+  if (props.data.pageBreakBefore) {
+    baseClasses += " alwaysbreak";
   }
-}
+
+  return (
+    <div className={baseClasses}>
+      <ComponentHeader {...props} />
+      <h6>{props.data.header}</h6>
+      <hr />
+    </div>
+  );
+};
+
+export default Section;
