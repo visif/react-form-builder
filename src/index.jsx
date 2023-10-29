01aa3a2e54f55067b3f40460f918a5a446e2d6f4
santobbu <santobbu@gmail.com>
2023-10-30 01:21:41 +0700

    Revert index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -12,113 +12,100 @@ import store from "./stores/store";
 import Registry from "./stores/registry";
 import { FormProvider } from "./context/form-context";
 
-class ReactFormBuilder extends React.Component {
-  constructor(props) {
-    super(props);
+const ReactFormBuilder = (props) => {
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      editMode: false,
-      editElement: null,
-    };
-    this.editModeOn = this.editModeOn.bind(this);
-  }
+  //   this.state = {
+  //     editMode: false,
+  //     editElement: null,
+  //   };
+  //   this.editModeOn = this.editModeOn.bind(this);
+  // }
 
-  editModeOn(data, e) {
-    e.preventDefault();
-    e.stopPropagation();
-    if (this.state.editMode) {
-      this.setState({ editMode: !this.state.editMode, editElement: null });
-    } else {
-      this.setState({ editMode: !this.state.editMode, editElement: data });
-    }
-  }
-
-  manualEditModeOff() {
-    if (this.state.editMode) {
-      this.setState({
-        editMode: false,
-        editElement: null,
-      });
-    }
-  }
-
-  // onPost () {
-  //   if (typeof this.props.onPost === 'function') {
-  //     this.props.onPost(data)
+  // editModeOn(data, e) {
+  //   e.preventDefault();
+  //   e.stopPropagation();
+  //   if (this.state.editMode) {
+  //     this.setState({ editMode: !this.state.editMode, editElement: null });
+  //   } else {
+  //     this.setState({ editMode: !this.state.editMode, editElement: data });
   //   }
   // }
 
-  render() {
-    const toolbarProps = {
-      showDescription: this.props.show_description,
-    };
-    if (this.props.toolbarItems) {
-      toolbarProps.items = this.props.toolbarItems;
-    }
-    return (
-      <DndProvider backend={HTML5Backend}>
-        <FormProvider>
-          <div>
-            {/* <div>
-           <p>
-             It is easy to implement a sortable interface with React DnD. Just make
-             the same component both a drag source and a drop target, and reorder
-             the data in the <code>hover</code> handler.
-           </p>
-           <Container />
-         </div> */}
-            <div className="react-form-builder clearfix">
-              <div>
-                <Preview
-                  files={this.props.files}
-                  manualEditModeOff={this.manualEditModeOff.bind(this)}
-                  showCorrectColumn={this.props.showCorrectColumn}
-                  parent={this}
-                  data={this.props.data}
-                  url={this.props.url}
-                  saveUrl={this.props.saveUrl}
-                  onLoad={this.props.onLoad}
-                  onPost={this.props.onPost}
-                  editModeOn={this.editModeOn}
-                  editMode={this.state.editMode}
-                  variables={this.props.variables}
-                  registry={Registry}
-                  editElement={this.state.editElement}
-                  renderEditForm={this.props.renderEditForm}
-                  onChange={this.props.onChange}
-                  uploadUrl={this.props.uploadUrl}
-                  onImageUpload={this.props.onImageUpload}
-                  getDataSource={this.props.getDataSource}
-                  getFormSource={this.props.getFormSource}
-                  getFormContent={this.props.getFormContent}
-                  getActiveUserProperties={() => {
-                    return {
-                      name: "test",
-                      userId: "id001",
-                    };
-                  }}
-                  onUploadFile={(file) => {
-                    return `${file.name}-${Math.random() * 10000000}`;
-                  }}
-                  onUploadImage={(file) => {
-                    return `path/${file.name}-${Math.random() * 10000000}`;
-                  }}
-                  onDownloadFile={(file) => {
-                    return `download_${file.name}-${Math.random() * 10000000}`;
-                  }}
-                />
-                <Toolbar
-                  {...toolbarProps}
-                  customItems={this.props.customToolbarItems}
-                />
-              </div>
+  // manualEditModeOff() {
+  //   if (this.state.editMode) {
+  //     this.setState({
+  //       editMode: false,
+  //       editElement: null,
+  //     });
+  //   }
+  // }
+
+  // render() {
+  const toolbarProps = {
+    showDescription: props.show_description,
+  };
+  if (props.toolbarItems) {
+    toolbarProps.items = props.toolbarItems;
+  }
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <FormProvider>
+        <div>
+          <div className="react-form-builder clearfix">
+            <div>
+              {/* <Preview
+                files={this.props.files}
+                manualEditModeOff={this.manualEditModeOff.bind(this)}
+                showCorrectColumn={this.props.showCorrectColumn}
+                parent={this}
+                data={this.props.data}
+                url={this.props.url}
+                saveUrl={this.props.saveUrl}
+                onLoad={this.props.onLoad}
+                onPost={this.props.onPost}
+                editModeOn={this.editModeOn}
+                editMode={this.state.editMode}
+                variables={this.props.variables}
+                registry={Registry}
+                editElement={this.state.editElement}
+                renderEditForm={this.props.renderEditForm}
+                onChange={this.props.onChange}
+                uploadUrl={this.props.uploadUrl}
+                onImageUpload={this.props.onImageUpload}
+                getDataSource={this.props.getDataSource}
+                getFormSource={this.props.getFormSource}
+                getFormContent={this.props.getFormContent}
+                getActiveUserProperties={() => {
+                  return {
+                    name: "test",
+                    userId: "id001",
+                  };
+                }}
+                onUploadFile={(file) => {
+                  return `${file.name}-${Math.random() * 10000000}`;
+                }}
+                onUploadImage={(file) => {
+                  return `path/${file.name}-${Math.random() * 10000000}`;
+                }}
+                onDownloadFile={(file) => {
+                  return `download_${file.name}-${Math.random() * 10000000}`;
+                }}
+              /> */}
+              <Toolbar
+                {...toolbarProps}
+                customItems={props.customToolbarItems}
+              />
             </div>
           </div>
-        </FormProvider>
-      </DndProvider>
-    );
-  }
-}
+        </div>
+      </FormProvider>
+    </DndProvider>
+  );
+  // }
+};
 
 const FormBuilders = {};
 FormBuilders.ReactFormBuilder = ReactFormBuilder;
