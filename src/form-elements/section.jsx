58e4b597fcd4bc331bf1969a65da3c7fc033a2e9
santobbu <santobbu@gmail.com>
2023-12-13 23:39:36 +0700

    Remove commented code

--- src/form-elements/section.jsx
+++ src/form-elements/section.jsx
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import ComponentHeader from './component-header';
-
-// export default class Section extends React.Component {
-//   render() {
-//     let baseClasses = 'SortableItem rfb-item';
-//     if (this.props.data.pageBreakBefore) { baseClasses += ' alwaysbreak'; }
-
-//     return (
-//       <div className={baseClasses}>
-//         <ComponentHeader {...this.props} />
-//         <h6>{this.props.data.header}</h6>
-//         <hr />
-//       </div>
-//     );
-//   }
-// }
-
 import React from "react";
 import ComponentHeader from "./component-header";
 
