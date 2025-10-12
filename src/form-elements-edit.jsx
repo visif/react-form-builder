d90484f30dc78d5bcb5faaa7ecbc4143a9589c0d
fortunealphat <fortunealphat@gmail.com>
2025-10-12 08:32:44 +0700

    Add color picker to Editor

--- src/form-elements-edit.jsx
+++ src/form-elements-edit.jsx
@@ -12,11 +12,23 @@ import ID from './UUID'
 import './styles/draft-align.css'
 
 const toolbar = {
-  options: ['inline', 'list', 'textAlign', 'fontSize', 'link', 'history'],
+  options: ['inline', 'list', 'textAlign', 'fontSize', 'link', 'colorPicker', 'history'],
   inline: {
     inDropdown: false,
     className: undefined,
     options: ['bold', 'italic', 'underline', 'superscript', 'subscript'],
+  },
+  colorPicker: {
+    className: 'rainbow-color-picker',  // Add this custom class
+    component: undefined,
+    popupClassName: undefined,
+    colors: ['rgb(97,189,109)', 'rgb(26,188,156)', 'rgb(84,172,210)', 'rgb(44,130,201)',
+             'rgb(147,101,184)', 'rgb(71,85,119)', 'rgb(204,204,204)', 'rgb(65,168,95)',
+             'rgb(0,168,133)', 'rgb(61,142,185)', 'rgb(41,105,176)', 'rgb(85,57,130)',
+             'rgb(40,50,78)', 'rgb(0,0,0)', 'rgb(247,218,100)', 'rgb(251,160,38)',
+             'rgb(235,107,86)', 'rgb(226,80,65)', 'rgb(163,143,132)', 'rgb(239,239,239)',
+             'rgb(255,255,255)', 'rgb(250,197,28)', 'rgb(243,121,52)', 'rgb(209,72,65)',
+             'rgb(184,49,47)', 'rgb(124,112,107)', 'rgb(209,213,216)'],
   },
 }
 
