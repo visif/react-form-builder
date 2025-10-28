60fca9e944eabbc4bdfc9649acc585d698c9860c
santobbu <santobbu@gmail.com>
2025-10-29 01:16:54 +0700

    refactor: convert HeaderBar to functional component
    Phase 10 - Part 2/3 continued
    
    - Converted HeaderBar to functional component
    - Simple presentational component with conditional rendering
    - Removed class-based this bindings
    
    Remaining for Phase 10:
    - Table (complex state management)
    - FormElementsEdit (edit panel)
    - ReactForm + FormValidator (form generator)

--- src/form-elements/header-bar.jsx
+++ src/form-elements/header-bar.jsx
@@ -4,36 +4,36 @@
 import React from 'react'
 import Grip from '../multi-column/grip'
 
-export default class HeaderBar extends React.Component {
-  render() {
-    return (
-      <div className="toolbar-header">
-        <span className="badge badge-secondary">{this.props.data.text}</span>
-        <div className="toolbar-header-buttons">
-          {this.props.data.element !== 'LineBreak' && (
-            <div
-              className="btn is-isolated"
-              onClick={this.props.editModeOn?.bind(this.props.parent, this.props.data)}
-            >
-              <i className="is-isolated fas fa-edit"></i>
-            </div>
-          )}
+const HeaderBar = (props) => {
+  return (
+    <div className="toolbar-header">
+      <span className="badge badge-secondary">{props.data.text}</span>
+      <div className="toolbar-header-buttons">
+        {props.data.element !== 'LineBreak' && (
           <div
             className="btn is-isolated"
-            onClick={this.props.onDestroy?.bind(this, this.props.data)}
+            onClick={props.editModeOn?.bind(props.parent, props.data)}
           >
-            <i className="is-isolated fas fa-trash"></i>
+            <i className="is-isolated fas fa-edit"></i>
           </div>
-          {!this.props.data.isContainer && (
-            <Grip
-              data={this.props.data}
-              index={this.props.index}
-              onDestroy={this.props.onDestroy}
-              setAsChild={this.props.setAsChild}
-            />
-          )}
+        )}
+        <div
+          className="btn is-isolated"
+          onClick={props.onDestroy?.bind(null, props.data)}
+        >
+          <i className="is-isolated fas fa-trash"></i>
         </div>
+        {!props.data.isContainer && (
+          <Grip
+            data={props.data}
+            index={props.index}
+            onDestroy={props.onDestroy}
+            setAsChild={props.setAsChild}
+          />
+        )}
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default HeaderBar
