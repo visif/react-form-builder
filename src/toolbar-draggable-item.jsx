b3c827cdd838e51a077a5170dbea9ea3813dc835
santobbu <santobbu@gmail.com>
2025-10-29 00:42:44 +0700

    refactor: convert ToolbarItem from class to functional component with react-dnd hooks
    - Converted from class component to functional component
    - Replaced DragSource HOC with useDrag hook (modern react-dnd API)
    - Added visual feedback with isDragging opacity
    - Updated to react-dnd 16.x hooks API
    - Phase 6 - Component 4/5

--- src/toolbar-draggable-item.jsx
+++ src/toolbar-draggable-item.jsx
@@ -2,34 +2,35 @@
  * <ToolbarItem />
  */
 import React from 'react'
-import { DragSource } from 'react-dnd'
+import { useDrag } from 'react-dnd'
 import ItemTypes from './ItemTypes'
 import ID from './UUID'
 
-const cardSource = {
-  beginDrag(props) {
-    return {
-      id: ID.uuid(),
-      index: -1,
-      data: props.data,
-      onCreate: props.onCreate,
-    }
-  },
+const ToolbarItem = ({ data, onClick, onCreate }) => {
+  const [{ isDragging }, connectDragSource] = useDrag(
+    () => ({
+      type: ItemTypes.CARD,
+      item: () => ({
+        id: ID.uuid(),
+        index: -1,
+        data,
+        onCreate,
+      }),
+      collect: (monitor) => ({
+        isDragging: monitor.isDragging(),
+      }),
+    }),
+    [data, onCreate]
+  )
+
+  if (!connectDragSource) return null
+
+  return connectDragSource(
+    <li onClick={onClick} style={{ opacity: isDragging ? 0.5 : 1 }}>
+      <i className={data.icon}></i>
+      {data.name}
+    </li>
+  )
 }
 
-class ToolbarItem extends React.Component {
-  render() {
-    const { connectDragSource, data, onClick } = this.props
-    if (!connectDragSource) return null
-    return connectDragSource(
-      <li onClick={onClick}>
-        <i className={data.icon}></i>
-        {data.name}
-      </li>
-    )
-  }
-}
-
-export default DragSource(ItemTypes.CARD, cardSource, (connect) => ({
-  connectDragSource: connect.dragSource(),
-}))(ToolbarItem)
+export default ToolbarItem
