436cfd6b04c8178156b75437058fd9e17e258156
santobbu <santobbu@gmail.com>
2026-01-21 00:29:53 +0700

    Refactor sortable element and multi-column components for improved drag-and-drop functionality

--- src/form-place-holder.jsx
+++ src/form-place-holder.jsx
@@ -5,10 +5,14 @@ const PLACE_HOLDER = 'form-place-holder'
 
 export default class PlaceHolder extends React.Component {
   render() {
+    const isEmpty = this.props.index === 0
     return (
       this.props.show && (
-        <div className={PLACE_HOLDER}>
-          <div>{this.props.text}</div>
+        <div
+          className={PLACE_HOLDER}
+          style={{ minHeight: isEmpty ? '100px' : '50px', opacity: isEmpty ? 1 : 0.5 }}
+        >
+          <div>{isEmpty ? this.props.text : 'Drop items here'}</div>
         </div>
       )
     )
