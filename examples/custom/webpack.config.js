35cf76954eb1fc75335b0c41d90a4ba23e69f62f
santobbu <santobbu@gmail.com>
2026-01-23 11:47:08 +0700

    Refactor webpack configuration files for consistency; add ProvidePlugin for process/browser

--- examples/custom/webpack.config.js
+++ examples/custom/webpack.config.js
@@ -1,4 +1,5 @@
-var path = require('path');
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
   entry: './app.js',
@@ -7,39 +8,44 @@ module.exports = {
     path: path.resolve('.'),
     filename: 'bundle.js',
     library: 'ReactFormBuilder',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
 
   externals: {
-    'bootstrap': 'bootstrap'
+    bootstrap: 'bootstrap',
   },
 
   resolve: {
     extensions: ['.js', '.jsx', '.scss', '.css', '.json'],
     alias: {
-      "jquery": path.join(__dirname, "./jquery-stub.js")
-    }
+      jquery: path.join(__dirname, './jquery-stub.js'),
+    },
   },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
 
   module: {
     rules: [
       {
         exclude: /node_modules/,
         test: /\.js|.jsx?$/,
-        use: [
-          { loader: 'babel-loader' }
-        ]
-      }, {
+        use: [{ loader: 'babel-loader' }],
+      },
+      {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: 'style-loader',
           },
           {
-            loader: 'css-loader'
+            loader: 'css-loader',
           },
-        ]
-      }
-    ]
-  }
-};
+        ],
+      },
+    ],
+  },
+}
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,14 +1,14 @@
-var path = require("path");
-var webpack = require("webpack");
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
-  entry: "./src/index.jsx",
+  entry: './src/index.jsx',
 
   output: {
-    path: path.resolve("./dist"),
-    filename: "app.js",
-    library: "ReactFormBuilder",
-    libraryTarget: "umd",
+    path: path.resolve('./dist'),
+    filename: 'app.js',
+    library: 'ReactFormBuilder',
+    libraryTarget: 'umd',
     umdNamedDefine: true,
   },
 
@@ -16,45 +16,51 @@ module.exports = {
     //don't bundle the 'react' npm package with our bundle.js
     //but get it from a global 'React' variable
     react: {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "react",
-      root: "React",
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
     },
-    "react-dom": {
-      commonjs: "react-dom",
-      commonjs2: "react-dom",
-      amd: "react-dom",
-      root: "ReactDOM",
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
     },
     // 'react-datepicker': 'react-datepicker',
     // 'classnames': 'classnames',
     // 'jquery': 'jquery',
-    bootstrap: "bootstrap",
+    bootstrap: 'bootstrap',
   },
 
   resolve: {
-    extensions: ["./mjs", ".js", ".jsx", ".scss", ".css", ".json"],
+    extensions: ['./mjs', '.js', '.jsx', '.scss', '.css', '.json'],
     alias: {
-      jquery: path.join(__dirname, "./jquery-stub.js"),
+      jquery: path.join(__dirname, './jquery-stub.js'),
     },
   },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
 
   module: {
     rules: [
       {
         exclude: /node_modules/,
         test: /\.js|.jsx?$/,
-        use: [{ loader: "babel-loader" }],
+        use: [{ loader: 'babel-loader' }],
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
           },
         ],
       },
@@ -62,16 +68,16 @@ module.exports = {
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
               sassOptions: {
-                includePaths: ["./node_modules"],
+                includePaths: ['./node_modules'],
               },
             },
           },
@@ -82,4 +88,4 @@ module.exports = {
   performance: {
     hints: false,
   },
-};
+}
