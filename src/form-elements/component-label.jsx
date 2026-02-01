c572996c17ab2a4c5a6cbc9e67b7b692e6e2a05a
fortunealphat <fortunealphat@gmail.com>
2026-02-01 09:04:03 +0700

    Fix underline text display <ins> tag in preview mode

--- src/form-elements/component-label.jsx
+++ src/form-elements/component-label.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import myxss from './myxss'
 
+const convertUnderlineToIns = (html) => {
+  return html.replace(/<u>/g, '<ins>').replace(/<\/u>/g, '</ins>')
+}
+
 const ComponentLabel = (props) => {
   // Don't render anything if either isShowLabel is false or hideLabel is true
   if (
@@ -36,6 +40,7 @@ const ComponentLabel = (props) => {
     !props.read_only
 
   let labelText = myxss.process(props.data.label)
+  labelText = convertUnderlineToIns(labelText)
   if (props.data.formularKey && props.preview) {
     labelText = `${labelText} (${props.data.formularKey})`
   }
