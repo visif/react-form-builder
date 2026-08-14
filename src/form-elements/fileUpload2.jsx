2120715bdcbc2b3d2382f199e19ad3126578b28a
fortunealphat <fortunealphat@gmail.com>
2026-08-14 17:41:37 +0700

    Change icon for fileupload and imageupload to be the same

--- src/form-elements/fileUpload2.jsx
+++ src/form-elements/fileUpload2.jsx
@@ -1,26 +1,8 @@
 import React from 'react'
+import { DeleteOutlined } from '@ant-design/icons'
 import ComponentHeader from './component-header'
 
 const getSavedEditor = (editor) => (Array.isArray(editor) ? editor[0] : editor)
-
-const TrashIcon = () => (
-  <svg
-    viewBox="0 0 24 24"
-    width="16"
-    height="16"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    aria-hidden="true"
-  >
-    <polyline points="3 6 5 6 21 6" />
-    <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" />
-    <path d="M10 11v6M14 11v6" />
-    <path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2" />
-  </svg>
-)
 
 class FileUpload extends React.Component {
   constructor(props) {
@@ -221,13 +203,14 @@ class FileUpload extends React.Component {
                             border: 'none',
                             padding: 4,
                             lineHeight: 1,
+                            fontSize: 18,
                             flexShrink: 0,
                           }}
                           onClick={() => {
                             this.onRemoveFile(file)
                           }}
                         >
-                          <TrashIcon />
+                          <DeleteOutlined />
                         </button>
                       )}
                     </li>
